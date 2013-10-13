ca4dec239d72c473ae03eace2238a2cdfad662ae
amytsai <amietsai@gmail.com>
2013-10-12 18:47:10 -0700

    fixed bug in generate ray

--- raytrace.cpp
+++ raytrace.cpp
@@ -641,8 +641,8 @@ void Camera::generateRay(Sample s, Ray* ray) {
     float y = -(s.y - ((float) height)/2);
 	printf("original value at: %f, %f \n", x, y);
     float theta = fov/2;
-    x = x*tan(theta)/((float) width)/2);
-    y = y*tan(theta)/((float) height)/2);
+    x = x*tan(theta)/(((float) width) / 2) ;
+    y = y*tan(theta)/(((float) height) / 2) ;
 	printf("scaled value at: %f, %f \n", x, y);
     float imagePlaneW = (UL.sub(UR)).len;
     float imagePlaneH = (UL.sub(LL)).len;
